f2fc652e002e30a857d3a8e21f2641cbe6a319ac
Khirushan-Sasitharan-ICD2O1-01 <khirushan.sasitharan@student.tdsb.on.ca>
2024-09-18 14:24:13 +0000

    dd

--- PP1_3.py
+++ PP1_3.py
@@ -1,17 +1,17 @@
 
 
 def q1():
-  print ( "* * * * * * \n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n" )
+  print ( " * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n" )
 
   print ("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
 
   print ("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 def q2():
   print ("Hello", end = "")  
-  print (" World\n")
+  print (" World")
 
 def q3():
-  print ('"Man"\n"board"\n"Man Overboard"')
+  print ('"Man"\n"Board"\n"Man Overboard"')
   
 
 
